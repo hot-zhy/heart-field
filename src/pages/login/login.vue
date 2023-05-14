28fd5b40f0d83ecaa1e8aa80af5d603030c4da39
yz <3424288785@qq.com>
2023-05-15 00:37:21 +0800

    提交人：yz 日期：5.15 行为：增加了login.vue的页面设计，新增了font和img在静态文件夹之下

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,6 +1,10 @@
 <template>
 	<view class="content" :style="{background: 'url('+backgroundURL+')'}">
-		登录页面
+		<text class="title">{{titleText}}</text>
+		<view class="info">
+			<view class="info1">{{infoTexts[0]}}</view>
+			<view class="info2">{{infoTexts[1]}}</view>
+		</view>
 	</view>
 </template>
 
@@ -8,7 +12,9 @@
 	export default {
 		data() {
 			return {
-				backgroundURL: '/static/yz-static/background.png'
+				backgroundURL: '/static/img/login/background.png',
+				titleText: '心灵绿野',
+				infoTexts: ['随心倾述', '自在畅聊']
 			}
 		},
 		methods: {
@@ -18,7 +24,31 @@
 </script>
 
 <style>
-.content {
-	height: 100vh;
-}
+	@import '@/static/font/ZKKL.css';
+	@import '@/static/font/YSBT.css';
+	.content {
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.title {
+		font-family: YSBT;
+		color: #5F6A58;
+		margin-top: 32vh;
+		font-size: 13vw;
+	}
+	.info {
+		font-family: ZKKL;
+		color: #5F6A58;
+		margin-top: 4vh;
+		font-size: 7vw;
+		height: 100vw;
+	}
+	.info1 {
+		padding-right: 30vw;
+	}
+	.info2 {
+		padding-left: 30vw;
+	}
 </style>
